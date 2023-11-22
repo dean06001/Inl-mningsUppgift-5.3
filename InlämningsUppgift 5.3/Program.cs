d48de153f1a39a09c8a1342d1ab19037ec2785a1
dean06001 <dean06001@E-PW0234KC>
2023-11-22 10:34:53 +0100

    Add project files.

---
+++ InlämningsUppgift 5.3/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace InlämningUppgift_5._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in tre heltal som du vill veta summan av");
+            int [] skrift = new int[3];
+            skrift[0]=int.Parse(Console.ReadLine());
+            skrift[1]=int.Parse(Console.ReadLine());
+            skrift[2]=int.Parse(Console.ReadLine());
+            int Tal1 = skrift[0];
+            int Tal2 = skrift[1];
+            int Tal3 = skrift[2];
+            
+            Console.WriteLine("Summan av talen du valt är: " + (Tal1 + Tal2 + Tal3));
+            Console.ReadKey();
+        }
+    }
+}
